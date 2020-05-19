99a379b3d2ec6ce58426424cf51c3aa657268e79
IB-C <64722938+IB-C@users.noreply.github.com>
2020-05-19 20:18:42 +0700

    Add files via upload

---
+++ js/javascripts.js
@@ -0,0 +1,49 @@
+function nasiGoreng() {
+    document.getElementById("nasigoreng").style.display = "block";
+    document.getElementById("body").style.overflowY = "hidden";
+}
+function ayamGoreng() {
+    document.getElementById("ayamgoreng").style.display = "block";
+    document.getElementById("body").style.overflowY = "hidden";
+}
+function satePadang() {
+    document.getElementById("satepadang").style.display = "block";
+    document.getElementById("body").style.overflowY = "hidden";
+}
+function igaBakar() {
+    document.getElementById("igabakar").style.display = "block";
+    document.getElementById("body").style.overflowY = "hidden";
+}
+function steak() {
+    document.getElementById("sirloin").style.display = "block";
+    document.getElementById("body").style.overflowY = "hidden";
+}
+function baksoSapi() {
+    document.getElementById("baksosapi").style.display = "block";
+    document.getElementById("body").style.overflowY = "hidden";
+}
+function pizza() {
+    document.getElementById("pizza").style.display = "block";
+    document.getElementById("body").style.overflowY = "hidden";
+}
+function nasiKuning() {
+    document.getElementById("nasikuning").style.display = "block";
+    document.getElementById("body").style.overflowY = "hidden";
+}
+function lasagna() {
+    document.getElementById("lasagna").style.display = "block";
+    document.getElementById("body").style.overflowY = "hidden";
+}
+
+function off() {
+    document.getElementById("nasigoreng").style.display = "none";
+    document.getElementById("ayamgoreng").style.display = "none";
+    document.getElementById("satepadang").style.display = "none";
+    document.getElementById("igabakar").style.display = "none";
+    document.getElementById("sirloin").style.display = "none";
+    document.getElementById("baksosapi").style.display = "none";
+    document.getElementById("pizza").style.display = "none";
+    document.getElementById("nasikuning").style.display = "none";
+    document.getElementById("lasagna").style.display = "none";
+    document.getElementById("body").style.overflowY = "auto";
+}
